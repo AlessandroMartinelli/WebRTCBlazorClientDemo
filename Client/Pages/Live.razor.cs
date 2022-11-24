91cd4834f81b5983142a22ff0bf21cb143d590a2
AMartinelli <a.martinelli1990@gmail.com>
2022-11-24 19:03:10 +0100

    Working video player demo

---
+++ Client/Pages/Live.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace WebRTCBlazorClientDemo.Client.Pages
+{
+    public partial class Live : ComponentBase
+    {
+        [Inject] public IJSRuntime Js { get; set; }
+
+        private string _videoId = "";
+
+        protected override Task OnInitializedAsync() {
+            return base.OnInitializedAsync();
+        }
+
+        protected override void OnInitialized() {
+            _videoId = "video";
+            base.OnInitialized();
+        }
+
+        protected async override Task OnAfterRenderAsync(bool firstRender) {
+            if (firstRender) {
+                string streamUrl = "http://localhost:8083/stream/e254df96-2a0e-4bbc-a146-7cb688fa9276/channel/0/webrtc";
+                await Js.InvokeAsync<string>("start_WebRTC_Player", streamUrl, _videoId);
+            }
+            await base.OnAfterRenderAsync(firstRender);
+        }
+    }
+}
---
+++ Client/wwwroot/Js/RTSPtoWEB.js
@@ -0,0 +1,48 @@
+﻿window.start_WebRTC_Player = (streamUrl, videoElementName) => {
+    let videoElement = document.getElementById(videoElementName);
+    startPlay(videoElement, streamUrl);
+};
+
+function startPlay(videoElement, streamUrl) {
+    const webrtc = new RTCPeerConnection({
+        iceServers: [],
+        sdpSemantics: 'unified-plan'
+    })
+    webrtc.ontrack = function (event) {
+        console.log(event.streams.length + ' track has been delivered')
+        videoElement.srcObject = event.streams[0]
+        videoElement.play()
+    }
+    webrtc.addTransceiver('video', { direction: 'sendrecv' })
+    webrtc.onnegotiationneeded = async function handleNegotiationNeeded() {
+        const offer = await webrtc.createOffer()
+
+        await webrtc.setLocalDescription(offer)
+
+        fetch(streamUrl, {
+            method: 'POST',
+            body: new URLSearchParams({ data: btoa(webrtc.localDescription.sdp) })
+        })
+            .then(response => response.text())
+            .then(data => {
+                try {
+                    webrtc.setRemoteDescription(
+                        new RTCSessionDescription({ type: 'answer', sdp: atob(data) })
+                    )
+                } catch (e) {
+                    console.warn(e)
+                }
+            })
+    }
+
+    const webrtcSendChannel = webrtc.createDataChannel('rtsptowebSendChannel')
+    webrtcSendChannel.onopen = (event) => {
+        console.log(`${webrtcSendChannel.label} opened`)
+        webrtcSendChannel.send('ping')
+    }
+    webrtcSendChannel.onclose = (_event) => {
+        console.log(`${webrtcSendChannel.label} closed`)
+        startPlay(videoElement, streamUrl)
+    }
+    webrtcSendChannel.onmessage = event => console.log(event.data)
+}
